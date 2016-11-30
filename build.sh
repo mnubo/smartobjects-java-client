75acbc9bc84301876aea2415467dff75cc824fb6
David Francoeur <dfrancoeur04@gmail.com>
2016-11-30 13:34:17 -0500

    fix(travis): decryption fails

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    openssl aes-256-cbc -K $encrypted_176ea4399631_key -iv $encrypted_176ea4399631_iv \
-      -in credentials.properties.enc -out src/test/resources/credentials.properties -d
+    openssl aes-256-cbc -K $encrypted_176ea4399631_key -iv $encrypted_176ea4399631_iv -in credentials.properties.enc -out src/test/resources/credentials.properties -d
     mvn clean test
 else
     # These test must be run on mnubo/smartobjects-java-client because they require the encrypted key
